8b8f2e6dc89d4ebe0c65075f4e32abf4e6b30718
San-tiw <131628777+San-tiw@users.noreply.github.com>
2023-04-24 07:06:35 +0530

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -1,6 +1,6 @@
 public class Addition {
     public static void main(String[] args) {
-        int a = 5;
+        int a = 10;
         int b = 10;
         int sum = a + b;
         System.out.println("The sum of " + a + " and " + b + " is " + sum);
